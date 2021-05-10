6e44252e4134101dbc26cd3c0571724658e01c49
Jason <jrosasrjf@gmail.com>
2021-05-09 23:20:46 -0500

    added comments to Product.js

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -9,11 +9,14 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    id: { 
-      type: DataTypes.INTEGER,
-      allowNull: true, 
-      primaryKey: true, 
-      autoIncrement: true 
+
+    // an array of objects. The objects being the table column properties. 
+
+    id: { // column heading is id.
+      type: DataTypes.INTEGER, // the kind of data accepted in this column.
+      allowNull: true, // input cannot be null (left empty).
+      primaryKey: true, // this is the primary key. This is the main grouping within this table. 
+      autoIncrement: true // increases the id count per entry. 
     },
 
     product_name: { 
@@ -55,7 +58,7 @@ Product.init(
     underscored: true,
     modelName: 'product',
   }
-  
+
 );
 
 module.exports = Product;
