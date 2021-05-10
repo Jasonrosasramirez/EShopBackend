53d76fd99514854b1974a53648f4f0dc7f968c70
Jason <jrosasrjf@gmail.com>
2021-05-09 21:51:22 -0500

    added force: false in the sync method

--- Develop/server.js
+++ Develop/server.js
@@ -13,17 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-sequelize.sync().then(() => {
+sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
 });
 
-
-
-/*
-originally found here. 
-
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
-});
-
-*/
