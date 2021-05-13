24888ca37be5d6711d314d857563aa7784a2ac94
Jason <jrosasrjf@gmail.com>
2021-05-13 03:02:17 -0500

    completed /:id section

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -53,6 +53,26 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+
+  try {
+    const tagData = await Tag.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!tagData) {
+      res.status(404).json({ message: "Page not found. ID Not correct :( " });
+      return;
+    }
+
+    res.status(200).json(tagData);
+  } 
+
+  catch (err) {
+    res.status(500).json(err);
+  }
+
 });
 
 module.exports = router;
