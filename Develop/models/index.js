d04e452c7cae9ead2e62e3a71ad065cad0479aaf
Jason <jrosasrjf@gmail.com>
2021-05-12 23:23:19 -0500

    added in belngto method to index.js

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -4,7 +4,16 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
+// This script joins the seperate scripts together. 
+
 // Products belongsTo Category
+Product.belongsTo(Category, { 
+  // product is the category
+  // belongsTo | automatically lookups an artical that has the asme id as the category_id column
+
+  foreignKey: "category_id", // refering to the main 
+});
+
 
 // Categories have many Products
 
