6ce2eeb534f817d0c39d1de2db909c6afc471fd5
Jason <jrosasrjf@gmail.com>
2021-05-13 01:53:42 -0500

    added in basic try method

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -58,7 +58,7 @@ router.post('/', async(req, res) => {
 
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async(req, res) => {
   // update a category by its `id` value
   try {
     const categoryDataMessage = await Category.update(req.body, {
--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -4,9 +4,22 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async(req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try {
+    const productDataMessage = await Product.findAll({
+      // search all searches multiple instances. 
+
+      include: [
+        { model: Category }, 
+        { model: Tag, 
+          through: ProductTag, 
+          as: "tagged_product" }]
+    });
+
+
+  }
 });
 
 // get one product
