a86191ad78dbf248d6e01213c408ef392f3ea588
Jason <jrosasrjf@gmail.com>
2021-05-13 00:13:09 -0500

    added in catch(err)

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -47,6 +47,15 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new category
+  try {
+    const categoryDataMessage = await Category.create(req.body);
+    res.status(200).json(categoryDataMessage); 
+  }
+
+  catch (err) {
+    res.status(400).json(err);
+  }
+  
 });
 
 router.put('/:id', (req, res) => {
