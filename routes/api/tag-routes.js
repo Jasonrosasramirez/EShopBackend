c48f7864f9a00427bdb03ca6fd53c7dc41a9a405
Jason <jrosasrjf@gmail.com>
2021-05-13 22:59:09 -0500

    edited the delete model

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -89,24 +89,15 @@ router.put('/:id', async(req, res) => {
 router.delete('/:id', async(req, res) => {
   // delete on tag by its `id` value
 
-  try {
-    const tagDataMessage = await Tag.destroy({
-      where: {
-        id: req.params.id
-      }
-    });
-
-    if (!tagDataMessage) {
-      res.status(404).json({ message: "Page not found. ID Not correct :( " });
-      return;
+  Tag.destroy({
+    where: {
+      id: req.params.id
     }
-
-    res.status(200).json(tagDataMessage);
-  } 
-
-  catch (err) {
-    res.status(500).json(err);
-  }
+  })
+  .then(
+    products => res.json(products)
+  )
+  .catch(err => res.json(err));
 
 });
 
