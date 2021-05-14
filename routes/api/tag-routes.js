e941ad14f2a92c25de182f89b1b06afbce3dccca
Jason <jrosasrjf@gmail.com>
2021-05-13 23:07:20 -0500

    further edits to the tag-routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -62,28 +62,47 @@ router.post('/', (req, res) => {
     })
 });
 
-router.put('/:id', async(req, res) => {
+router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
-  try {
-    const tagDataMessage = await Tag.create(req.body, {
-      where: {
-        id: req.params.id,
-      }
-    }); 
 
-    if (!tagDataMessage) {
-      res.status(404).json({message : "Hey look! The page is missing 404"});
-      return;
-    }; 
+  Tag.create(req.body, {
+    where: {
+      id: req.params.id,
+    },
+    })
+    .then((tag_created) => {
+      return ProductTag.findAll({
+        where: { product_id: req.params.id } }
+      );
+    })
+    .then((productTags) => {
+      // get list of current tag_ids
+      const productTagIds = productTags.map(({ tag_id }) => tag_id);
+      // create filtered list of new tag_ids
+      const newProductTags = req.body.tagIds
+        .filter((tag_id) => !productTagIds.includes(tag_id))
+        .map((tag_id) => {
+          return {
+            product_id: req.params.id,
+            tag_id,
+          };
+        });
+      // figure out which ones to remove
+      const productTagsToRemove = productTags
+        .filter(({ tag_id }) => !req.body.tagIds.includes(tag_id))
+        .map(({ id }) => id);
 
-    res.status(200).json(tagDataMessage)
-  }
-
-  catch (err) {
-    res.status(500).json(err);
-
-  }
-
+      // run both actions
+      return Promise.all([
+        ProductTag.destroy({ where: { id: productTagsToRemove } }),
+        ProductTag.bulkCreate(newProductTags),
+      ]);
+    })
+    .then((updatedProductTags) => res.json(updatedProductTags))
+    .catch((err) => {
+      // console.log(err);
+      res.status(400).json(err);
+    });
 });
 
 router.delete('/:id', async(req, res) => {
