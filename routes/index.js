e3b7631144bf91d322cec842d0bd3e2b3cb074e0
Jason <jrosasrjf@gmail.com>
2021-07-14 01:31:29 -0500

    updated readme

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,12 @@ const apiRoutes = require('./api');
 
 router.use('/api', apiRoutes);
 
+router.use("/", /* home page. Otherwise it just uses thr wrong woute */)
+
+
 router.use((req, res) => {
-  res.send("<h1>Wrong Route!</h1>")
+  res.send("<h1>Wrong Route! For E-Commerce Backend</h1>")
 });
 
+/* -- Exporting -- */
 module.exports = router;
